12fc5d18829f7a8433a23a7d440454ac3e797ac2
RobertoCV7 <robertocortesvalyh@gmail.com>
2025-10-29 15:47:12 -0600

    Parcial03 - Add member resolver

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,6 +11,7 @@ import { ServerError } from '../shared/errors/server-error/server-error';
 import { MemberProfile } from '../features/member-profile/member-profile';
 import { MemberPhotos } from '../features/member-photos/member-photos';
 import { MemberMessages } from '../features/member-messages/member-messages';
+import { memberResolver } from '../features/members/member-resolver';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -22,6 +23,8 @@ export const routes: Routes = [
       { path: 'members', component: MemberList },
       {
         path: 'members/:id',
+        resolve: { member: memberResolver },
+        runGuardsAndResolvers: 'always',
         component: MemberDetail,
         children: [
           { path: '', redirectTo: 'profile', pathMatch: 'full' },
--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -22,29 +22,21 @@ export class MemberDetail implements OnInit {
   private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+      next: (data) => this.member.set(data['member']),
+    });
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
+      filter((event) => event instanceof NavigationEnd)
     ).subscribe({
       next: () => {
         this.title.set(this.route.firstChild?.snapshot?.title);
-      }
-    })
-  }
-
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get('id');
-
-    if (id) {
-      return this.membersService.getMember(id);
-    }
-
-    return;
+      },
+    });
   }
 }
