04e96464f205d94e8ce5490fe0ecea63fbd2aab2
RobertoCV7 <robertocortesvalyh@gmail.com>
2026-03-19 14:42:25 -0600

    Parcial05 - Add filtering to API

--- src/API/Controllers/MembersController.cs
+++ src/API/Controllers/MembersController.cs
@@ -15,9 +15,9 @@ public class MembersController(IMembersRepository membersRepository,
     IPhotoService photoService) : BaseApiController
 {
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers([FromQuery] PaginationRequest paginationRequest)
-    {
-        return Ok(await membersRepository.GetMembersAsync(paginationRequest));
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMembers([FromQuery] MemberRequest request)
+    {   
+        return Ok(await membersRepository.GetMembersAsync(request));
     }
 
     [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
