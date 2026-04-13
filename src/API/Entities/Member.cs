c6aad1d8a803e4f43da8661031e670b426be1f4e
RobertoCV7 <robertocortesvalyh@gmail.com>
2026-04-13 10:49:08 -0600

    Parcial06 - Add initial migration for message

--- src/API/Entities/Member.cs
+++ src/API/Entities/Member.cs
@@ -26,7 +26,13 @@ public class Member
 
     [JsonIgnore]
     public List<MemberLike> LikedByMembers { get; set; } = [];
-    
+
     [JsonIgnore]
     public List<MemberLike> LikedMembers { get; set; } = [];
+
+    [JsonIgnore]
+    public List<Message> MessagesSent { get; set; } = [];
+
+    [JsonIgnore]
+    public List<Message> MessagesReceived { get; set; } = [];
 }
