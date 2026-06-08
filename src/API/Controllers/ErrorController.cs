957fc003725f0680758bc3504ac57b3685a803ed
RobertoCV7 <robertocortesvalyh@gmail.com>
2026-06-08 10:29:20 -0600

    Parcial06 - IDENTITY - Update token service

--- src/API/Controllers/ErrorController.cs
+++ src/API/Controllers/ErrorController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -29,6 +30,13 @@ public class ErrorController : BaseApiController
     public IActionResult GetServerError() // 500
     {
         throw new Exception("Server error");
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("admin-secret")]
+    public ActionResult<string> GetSecretAdmin()
+    {
+        return Ok("Only admins can see this");
     }
 }
 
