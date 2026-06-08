cd6350d2e73982210e662ad3a0ce6355d71a00b0
RobertoCV7 <robertocortesvalyh@gmail.com>
2026-06-08 10:00:04 -0600

    Parcial06 - IDENTITY - Fix breaking change in Identity

---
+++ src/API/Data/Migrations/20260608155647_IdentityUpdated.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "797905bc-96fa-4d21-939a-edfb598dd95c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "882f08a3-81dd-44dc-91cb-a7c67147494a");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "ebb9274b-af35-45a2-abf9-8075a4bfc997");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "589a5431-0554-4e7a-a873-71c5fcc4ab10");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "b67e2458-e560-45a3-b0e1-3313d9671a45");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "75fd0f9c-232c-437e-b140-e521be8461e0");
+        }
+    }
+}
