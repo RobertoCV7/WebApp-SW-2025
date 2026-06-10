349a15e5f86431877e5877563f4bce5262dffd3f
RobertoCV7 <robertocortesvalyh@gmail.com>
2026-06-10 09:23:14 -0600

    Parcial06 - SignalR - Update toast for new message notification

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -78,6 +78,7 @@ export class AccountService {
     localStorage.removeItem('filters');
     this.likesService.clearLikeIds();
     this.currentUser.set(null);
+    this.presenceService.stopHubConnection();
   }
 
   private getRolesFromToken(user: User): string[] {
--- client/src/core/services/presence-service.ts
+++ client/src/core/services/presence-service.ts
@@ -37,7 +37,12 @@ export class PresenceService {
     });
 
     this.hubConnection.on('NewMessageReceived', (message: Message) => {
-      this.toast.info(message.senderDisplayName + ' sent you a message');
+      this.toast.info(
+        message.senderDisplayName + ' sent you a message',
+        10000,
+        message.senderImageUrl,
+        `/members/${message.senderId}/messages`,
+      );
     });
   }
 
